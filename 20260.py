2a2c23e7112edca6707fd77e68fbf76299baa07d
Brian Serrano <benlexhi@gmail.com>
2026-04-07 11:11:17 -0400

    update code for transform to adjust perspective (20260.py)

--- 20260.py
+++ 20260.py
@@ -1,16 +1,16 @@
 import cv2
 import numpy as np
 
-camera = cv2.imread("testimage.jpg")
+camera = cv2.imread("testimage_Spring2026_yellow.jpg")
 cv2.imshow("Original frame", camera)
 
-camera_copy = cv2.imread("testimage.jpg")
+camera_copy = cv2.imread("testimage_Spring2026_yellow.jpg")
 box = cv2.rectangle(camera_copy, (0, 260), (640, 400), (0, 255, 0), 0)
 cv2.imshow("Rectangle", box)
 
 #define points array for transform
-pts1 = np.float32([[10, 20], [50, 20], [10, 20]])
-pts2 = np.float32([[10, 20], [50, 20], [10, 20]])
+pts1 = np.float32([[0, 260], [640, 260], [0, 400], [640, 400]])
+pts2 = np.float32([[0, 0], [480, 0], [0, 640], [480, 640]])
 
 #Apply perspective transform alg.
 matrix = cv2.getPerspectiveTransform(pts1, pts2)
