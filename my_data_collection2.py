2075d5ed4532c72deee929e38291e589954d1f1f
Benlexhi <38540386+Benlexhi@users.noreply.github.com>
2026-05-01 19:27:18 -0400

    iterate through 20 scans and save them

--- my_data_collection2.py
+++ my_data_collection2.py
@@ -8,6 +8,10 @@ import imutils
 import os
 from picamera2 import Picamera2
 
+scans = 20
+
+os.makedirs('/home/bserrano/ENME435/test_scans', exist_ok=True)
+
 # allow the camera to setup
 picam2 = Picamera2()
 config = picam2.create_video_configuration(
@@ -18,31 +22,32 @@ picam2.configure(config)
 picam2.start()
 time.sleep(1)
 
-# Enter the initial IMU angle from user
-d = input("Please enter IMU angle: ")
-print("Confirming the IMU angle you entered is: ")
-print(d)
+for i in range(scans):
 
-# Capture image directly using Picamera2 (replaces raspistill)
-image = picam2.capture_array()
-image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)  # Picamera2 outputs RGB, OpenCV expects BGR
-image = cv2.flip(image, -1)
+    # Enter the initial IMU angle from user
+    d = input(f"Please enter IMU angle for scan {i + 1}/{scans}: ")
+    print("Confirming the IMU angle you entered is: ")
+    print(d)
 
-# plot crosshairs for alignment
-cv2.line(image, (640, 0), (640, 720), (0, 150, 150), 1)
-cv2.line(image, (600, 360), (1280, 360), (0, 150, 150), 1)
+    # Capture image directly using Picamera2 (replaces raspistill)
+    image = picam2.capture_array()
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)  # Picamera2 outputs RGB, OpenCV expects BGR
+    image = cv2.flip(image, -1)
 
-# display IMU angle, for reference
-font = cv2.FONT_HERSHEY_COMPLEX_SMALL
-red = (0, 0, 255)
-cv2.putText(image, d, (800, 200), font, 10, red, 10)
+    # plot crosshairs for alignment
+    cv2.line(image, (640, 0), (640, 720), (0, 150, 150), 1)
+    cv2.line(image, (600, 360), (1280, 360), (0, 150, 150), 1)
 
-# write image to file
-d = int(d)
-filename = "%d.jpg" % d
-cv2.imwrite(filename, image)
+    # display IMU angle, for reference
+    font = cv2.FONT_HERSHEY_COMPLEX_SMALL
+    red = (0, 0, 255)
+    cv2.putText(image, d, (800, 200), font, 10, red, 10)
 
-os.makedirs('/home/bserrano/ENME435/test_scans', exist_ok=True)
-os.rename(filename, '/home/bserrano/ENME435/test_scans/' + filename)  # replaces os.system mv
+    # write image to file
+    d = int(d)
+    filename = "%d.jpg" % d
+    cv2.imwrite(filename, image)
 
-print("All done!")
+    os.rename(filename, '/home/bserrano/ENME435/test_scans/' + filename)  # replaces os.system mv
+
+    print("All done!")
